a0df0137826ea25b1a51e5a57e6a25b951a7e16d
aayushsynth <aayush.pat99@gmail.com>
2024-02-14 13:13:30 +0530

    modified test_1.py

--- test_1.py
+++ test_1.py
@@ -2,4 +2,5 @@ print("hello")
 #git diff  -> compares working tree to the staging area
 #git diff --staged  -> compares working tree to the last commit
 #git branch <brach_name>  --> to create a branch
+#git checkout <branch_name>  --> to navigate to the branch
 #sgs
