14c14060c04cb5586b22183188aec3091fb6da68
aayushsynth <aayush.pat99@gmail.com>
2024-02-18 10:54:29 +0530

    adding git commands in test_1.py

--- test_1.py
+++ test_1.py
@@ -1,4 +1,10 @@
-print("hello")
+print("hello this is a tutorial on git commands")
+#git init --> to make any repo a git repo
+#git status  --> to check which files are staged or not
+#git add --a  --> to push all files in the staged area
+#git commit -m "some meaningful message"  --> to commit all files in the staged area
+#git commit -a -m "some meaningful message"  --> to commit all files directly from working area 
+#.gitignore  -->inside this file you can add any files/folders which you want to ignore while staging
 #git diff  -> compares working tree to the staging area
 #git diff --staged  -> compares working tree to the last commit
 #git branch <brach_name>  --> to create a branch
