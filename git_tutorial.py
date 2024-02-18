107e8dc0819dc317d196eea091df01a094112871
aayushsynth <aayush.pat99@gmail.com>
2024-02-18 20:15:35 +0530

    updating the git_tutorial.py file

--- git_tutorial.py
+++ git_tutorial.py
@@ -6,6 +6,7 @@ print("hello this is a tutorial on git commands")
 #git add .  --> to push all files in the staged area
 #git commit -m "some meaningful message"  --> to commit all files in the staged area
 #git commit -a -m "some meaningful message"  --> to commit all files directly from working area 
+#rm -rf .git   --> to delete the git repository
 
 #IGNORING FILES
 #.gitignore  -->inside this file you can add any files/folders which you want to ignore while staging
@@ -27,10 +28,21 @@ print("hello this is a tutorial on git commands")
 #git checkout -f  --> to restore all files from the last commit
 
 
+#WORKING WITH REMOTE REPOS
+#After making a repo in github run the following command
+#git remote add origin_1 https://github.com/aayushsynth/My_Second_Repository.git  -->to link your git repo with your local directory through origin_1
+#Here the name origin_1 and the url may change a/c to the project
+#git push -u origin_1 master  --> to push the branch "master" from local repo to github (origin_1)
 
-  
 
+#BRANCHING IN GIT
 #git branch <brach_name>  --> to create a branch
 #git checkout <branch_name>  --> to navigate to the branch
 #git checkout -b <branch_name>  --> to create a branch and move into it
-# #sgs
+#git branch -v  --> shows the commit id and last commit message of all branches
+#git branch --merged  --> shows all the branches that are merged already
+#git branch --no-merged  --> shows all the branches that are not merged already
+#git branch -d <branch_name>  --> to delete a branch
+
+#MERGING TWO BRANCHES
+#git merge <branch_name>  --> to merge <branch_name> in the current branch you are in
